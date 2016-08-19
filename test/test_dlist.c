db04a534921cb19e18da26f2383d73d6f832e4f8
afiskon <afiskon@gmail.com>
2016-08-19 15:06:49 +0300

    Using asserts in test_dlist and test_slist

--- test/test_dlist.c
+++ test/test_dlist.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "ilist.h"
 
 typedef struct
@@ -16,36 +17,42 @@ int main()
 	dlist_iter iter;
 	ListItemData item1, item2, item3;
 	ListItem tmp;
+	char temp_buff[512];
 
 	dlist_head head;
 	dlist_init(&head);
+	assert(dlist_is_empty(&head));
 
 	strcpy(item1.data, "first item");
 	strcpy(item2.data, "second item");
 	strcpy(item3.data, "third item");
 
-	printf("is empty before: %d\n", dlist_is_empty(&head));
-
 	dlist_push_tail(&head, (dlist_node*) &item1);
+	assert(!dlist_is_empty(&head));
+
 	dlist_push_tail(&head, (dlist_node*) &item2);
 	dlist_push_tail(&head, (dlist_node*) &item3);
 
-	printf("is empty after: %d\n", dlist_is_empty(&head));
-
+	temp_buff[0] = 0;
 	dlist_foreach(iter, &head)
 	{
 		tmp = (ListItem) iter.cur;
-		printf("tmp->data = %s\n", tmp->data);
+		strcat(temp_buff, tmp->data);
+		strcat(temp_buff, ",");
 	}
+	assert(strcmp(temp_buff, "first item,second item,third item,") == 0);
 
 	tmp = (ListItem) dlist_pop_tail_node(&head);
-	printf("After dlist_pop_tail_node call tmp->data = %s\n", tmp->data);
+	assert(strcmp(tmp->data, "third item") == 0);
 
+	temp_buff[0] = 0;
 	dlist_foreach(iter, &head)
 	{
 		tmp = (ListItem) iter.cur;
-		printf("tmp->data = %s\n", tmp->data);
+		strcat(temp_buff, tmp->data);
+		strcat(temp_buff, ",");
 	}
+	assert(strcmp(temp_buff, "first item,second item,") == 0);
 
 	return 0;
 }
--- test/test_slist.c
+++ test/test_slist.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "ilist.h"
 
 typedef struct
@@ -16,36 +17,42 @@ int main()
 	slist_iter iter;
 	ListItemData item1, item2, item3;
 	ListItem tmp;
+	char temp_buff[512];
 
 	slist_head head;
 	slist_init(&head);
+	assert(slist_is_empty(&head));
 
 	strcpy(item1.data, "first item");
 	strcpy(item2.data, "second item");
 	strcpy(item3.data, "third item");
 
-	printf("is empty before: %d\n", slist_is_empty(&head));
-
 	slist_push_head(&head, (slist_node*)&item1);
+	assert(!slist_is_empty(&head));
+
 	slist_push_head(&head, (slist_node*)&item2);
 	slist_push_head(&head, (slist_node*)&item3);
 
-	printf("is empty after: %d\n", slist_is_empty(&head));
-
+	temp_buff[0] = 0;
 	slist_foreach(iter, &head)
 	{
 		tmp = (ListItem) iter.cur;
-		printf("tmp->data = %s\n", tmp->data);
+		strcat(temp_buff, tmp->data);
+		strcat(temp_buff, ",");
 	}
+	assert(strcmp(temp_buff, "third item,second item,first item,") == 0);
 
 	tmp = (ListItem) slist_pop_head_node(&head);
-	printf("After slist_pop_head_node call tmp->data = %s\n", tmp->data);
+	assert(strcmp(tmp->data, "third item") == 0);
 
+	temp_buff[0] = 0;
 	slist_foreach(iter, &head)
 	{
 		tmp = (ListItem) iter.cur;
-		printf("tmp->data = %s\n", tmp->data);
+		strcat(temp_buff, tmp->data);
+		strcat(temp_buff, ",");
 	}
+	assert(strcmp(temp_buff, "second item,first item,") == 0);
 
 	return 0;
 }
