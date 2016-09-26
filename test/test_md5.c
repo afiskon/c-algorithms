3b86a117d6d7676c90b60a9479ec20d58d78f78e
Aleksander Alekseev <afiskon@gmail.com>
2016-09-26 17:34:07 +0300

    md5 tests - more test vectors

--- test/test_md5.c
+++ test/test_md5.c
@@ -27,31 +27,29 @@ int main()
 	md5_ctxt ctx;
 	uint8_t res[MD5_RESULT_LEN];
 	char strres[MD5_RESULT_LEN*2 + 1];
+	char* vectors[] = {
+		"D41D8CD98F00B204E9800998ECF8427E", "",
+		"0CC175B9C0F1B6A831C399E269772661", "a",
+		"900150983CD24FB0D6963F7D28E17F72", "abc",
+		"F96B697D7CB7938D525A2F31AAF161D0", "message digest",
+		"C3FCD3D76192E4007DFB496CCA67E13B", "abcdefghijklmnopqrstuvwxyz",
+		"8215EF0796A20BCAAAE116D3876C664A", "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
+		"D174AB98D277D9F5A5611C2C9F419D9F", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+		"57EDF4A22BE3C955AC49DA2E2107B67A", "12345678901234567890123456789012345678901234567890123456789012345678901234567890",
+		NULL
+	};
+	int i = 0;
 
-	// Vector 0. message="" (empty string)
-
-	MD5Init(&ctx);
-	MD5Final(res, &ctx);
-	bytesToHex(res, strres);
-	printf("Vector 0, result = %s\n", strres);
-	assert(strcmp(strres, "D41D8CD98F00B204E9800998ECF8427E") == 0);
-
-	// Vector 1. message="a"
-	MD5Init(&ctx);
-	MD5Update(&ctx, (uint8_t*)"a", 1);
-	MD5Final(res, &ctx);
-	bytesToHex(res, strres);
-	printf("Vector 1, result = %s\n", strres);
-	assert(strcmp(strres, "0CC175B9C0F1B6A831C399E269772661") == 0);
-
-	// Vector 2. message="abc"
-	MD5Init(&ctx);
-	MD5Update(&ctx, (uint8_t*)"abc", 3);
-	MD5Final(res, &ctx);
-	bytesToHex(res, strres);
-	printf("Vector 2, result = %s\n", strres);
-	assert(strcmp(strres, "900150983CD24FB0D6963F7D28E17F72") == 0);
-
+	while(vectors[i] != NULL)
+	{
+		MD5Init(&ctx);
+		MD5Update(&ctx, (uint8_t*)vectors[i+1], strlen(vectors[i+1]));
+		MD5Final(res, &ctx);
+		bytesToHex(res, strres);
+		printf("Vector %i, result = %s, expected = %s\n", i / 2, strres, vectors[i]);
+		assert(strcmp(strres, vectors[i]) == 0);
+		i += 2;
+	}
 
 	return 0;
 }
